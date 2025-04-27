06b83e8806e6eb6e9e62f52aab64d23f612bfcf8
Hamza-S79 <hamzasalahuddin79@gmail.com>
2025-04-27 19:53:05 +0500

    A LOT
    ADDED-- New Folders for organization, created a Side-Bars template for all the pages, Created two style files, Created a Button template

--- TravelEase Project/ProgramFiles/SeacrhQueries.sql
+++ TravelEase Project/ProgramFiles/SeacrhQueries.sql
@@ -1,6 +1,5 @@
 Select * from Announced_Trip A
-JOIN Itinerary I on I.trip_design_id = A.trip_design_id
-where I.location like '%Bali'
+where A.accessibility_tags like '%wheel%'
 
 Select * from Announced_Trip A
 join Itinerary I on I.trip_design_id = A.trip_design_id
