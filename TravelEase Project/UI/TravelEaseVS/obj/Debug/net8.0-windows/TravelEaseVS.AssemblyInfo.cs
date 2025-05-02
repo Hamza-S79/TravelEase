7d94860c95c2975d82b3f009f4a9b4e2f6236530
Hamza-S79 <hamzasalahuddin79@gmail.com>
2025-05-02 16:50:03 +0500

    merge fix -login signup fixed

--- TravelEase Project/UI/TravelEaseVS/obj/Debug/net8.0-windows/TravelEaseVS.AssemblyInfo.cs
+++ TravelEase Project/UI/TravelEaseVS/obj/Debug/net8.0-windows/TravelEaseVS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelEaseVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49421ade1741fb4dff0c82634970e46249d312b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cfdb5d2d6d196363cce8f0caee150b3896ae503")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelEaseVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelEaseVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
